644de9cf40c212af9b9dc4ca7ef7f9996cb0e7b3
luca-masera <lucamasera1995@gmail.com>
2023-10-25 17:25:43 +0200

    array snack 4

--- Javascript/script.js
+++ Javascript/script.js
@@ -111,16 +111,53 @@ let bici = [
 ]
 let pesoBici = '15 kg'
 
+
+
 const pesoMinore= bici.filter ((element) =>{
        
       return element.peso < pesoBici
     
 })
 console.log(pesoMinore)
-
 const{ name, peso } = pesoMinore;
+console.log( name, peso)
 
 
 let html = document.getElementById('scrittura');
 html.innerHTML = ` La bici ${name} è la più leggera di tutte con un peso di ${peso}`;
 
+/*SNACK 4
+Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti.
+Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.*/
+
+
+let squadreCalcio = [
+    
+    {
+        nome: 'Juventus',
+        puntiFatti : 0,
+        falliSubiti : 0
+    },
+    {
+        nome: 'Inter',
+        puntiFatti : 0,
+        falliSubiti : 0
+    },
+    {
+        nome: 'Milan',
+        puntiFatti : 0,
+        falliSubiti : 0
+    },
+    {
+        nome: 'Sassuolo',
+        puntiFatti : 0,
+        falliSubiti : 0
+    },
+    {
+        nome: 'Napoli',
+        puntiFatti : 0,
+        falliSubiti : 0
+    }
+]
